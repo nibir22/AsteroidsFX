725314963f72da7264ffae6a995cfd3c82249f08
nibir22 <nibir22@student.sdu.dk>
2024-05-12 13:20:02 +0200

    Removed all unnecessary imports
    As name implied, forgot to remove before

--- Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
+++ Core/src/main/java/dk/sdu/mmmi/cbse/main/Main.java
@@ -1,23 +1,7 @@
 package dk.sdu.mmmi.cbse.main;
 
-import dk.sdu.mmmi.cbse.common.data.Entity;
-import dk.sdu.mmmi.cbse.common.data.GameData;
-import dk.sdu.mmmi.cbse.common.data.GameKeys;
-import dk.sdu.mmmi.cbse.common.data.World;
-import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
-import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
-import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
-
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import static java.util.stream.Collectors.toList;
-import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-import javafx.scene.layout.Pane;
-import javafx.scene.shape.Polygon;
-import javafx.scene.text.Text;
+
 import javafx.stage.Stage;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
